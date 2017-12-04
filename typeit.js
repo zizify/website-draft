a085e7062c8a9f475b257b7e9b92c785af2706e4
Andy Gaines <andyagaines@gmail.com>
2017-12-04 17:01:55 -0600

    first commit

---
+++ typeit.js
@@ -0,0 +1,8 @@
+new TypeIt('.typewriter', {
+	strings: ['Andy Gaines', '앤디 게인쓰', 'Анди Гейнз', 'Άνδι Γκέινς', 'アンディ・ゲインズ', 'אנדי גיינס'],
+	speed: 120,
+	deleteSpeed: 80,
+	loop: true,
+	breakLines: false,
+	autoStart: false
+});
